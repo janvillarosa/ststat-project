413868134c7e3fd916f95909db26cf6a7521a3ee
micaela-mangubat <asus@192.168.1.101>
2013-11-14 11:46:53 +0800

    rename class

---
+++ STSTATProject/src/advstatmp1/SampleDistribution.java
@@ -0,0 +1,72 @@
+package advstatmp1;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
+public class SampleDistribution{
+
+	//input x, n, N, k
+	//or input x0,...,xk, a0,...,ak, n, N
+        private ArrayList<Integer> population;
+        
+	
+	public BigInteger computeFactorial(int num){
+	
+		BigInteger factorial = BigInteger.valueOf(1);
+		
+		for(int i=1; i<=num;i++){
+                    factorial = factorial.multiply(BigInteger.valueOf(i));
+		}
+		return factorial;
+	}
+	
+	public double computeCombination(int n, int r){
+	
+                BigInteger combination = computeFactorial(n);
+                combination = combination.divide(computeFactorial(r).multiply(computeFactorial(n-r)));
+		return combination.doubleValue();
+
+        }
+        
+        public double computeTotalProbability(double[] probabilities){
+            
+            double total = 0.0;
+            for(int i = 0; i< probabilities.length; i++){
+                total += probabilities[i];
+            }
+            
+            return total;
+        }
+        
+        public ArrayList generateRandomSample(int size){
+            ArrayList<Integer> samples = new ArrayList();
+            
+            for(int i=0; i<size; i++){
+                samples.add(population.get((int)Math.random()%population.size()));
+            }
+            
+            return samples;
+        }
+
+        public DefaultTableModel getTabularForm(double[] probabilities, int[] x){
+            
+            DefaultTableModel model = new DefaultTableModel();
+            model.addColumn("x");
+            
+            for(int i = 0; i< x.length; i++){
+                model.addColumn(""+x[i]);
+            }
+            
+            ArrayList<String> p = new ArrayList(); 
+            p.add("f(x)");
+            for(int i = 0; i< probabilities.length; i++){
+                p.add(""+probabilities[i]);
+            }
+            String[] row = p.toArray(new String[p.size()]);
+            model.addRow(row);
+            
+            return model;
+        }
+        
+}
